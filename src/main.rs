aa60c6909a1b8991eb1145864ff3d060f442aca6
RodogInfinite <wjroland@gmail.com>
2022-10-26 15:30:18 -0400

    Cleanup and add working examples

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 use type_cast_macro::DataCast;
-use type_cast_macro_derive::cast;
 use type_cast_macro_derive::DataCast;
-use strum::IntoEnumIterator;
-use strum_macros::EnumIter;
 
 #[derive(Clone, Copy, Debug, DataCast)]
 pub enum DataTypes {
@@ -12,20 +9,24 @@ pub enum DataTypes {
     // 8 bytes
     #[cast(f64 => from_le_bytes)]
     IEEE754LSBDouble,
-    // 4 bytes
+    // [4 bytes, 4 bytes]
     #[cast([f32;2] => from_le_bytes)]
     IEEE754LSBSingleArr,
-    // 8 bytes
+    // [8 bytes, 8 bytes]
     #[cast([f64;2] => from_le_bytes)]
     IEEE754MSBDoubleArr,
 }
 
 
 fn main() {
-    let mut data: &[u8] = &[172, 152, 111, 195];
-    let mut data2: &[u8] = &[172, 152, 111, 195, 117, 93, 133, 192];
-    
-        DataTypes::IEEE754LSBSingle.parse(&mut data);
-    
+    let mut data:   &[u8] = &[172, 152, 111, 195];
+    let mut data2:  &[u8] = &[172, 152, 111, 195, 117, 93, 133, 192];
+    let mut data3:  &[u8] = &[172, 152, 111, 195, 117, 93, 133, 192, 172, 152, 111, 195, 117, 93, 133, 192];
+    println!("{:?}\n{:?}\n{:?}\n{:?}\n",
+        DataTypes::IEEE754LSBSingle.parse(&mut data),
+        DataTypes::IEEE754LSBDouble.parse(&mut data2),
+        DataTypes::IEEE754LSBSingleArr.parse(&mut data2),
+        DataTypes::IEEE754MSBDoubleArr.parse(&mut data3),
+)
     
 }
