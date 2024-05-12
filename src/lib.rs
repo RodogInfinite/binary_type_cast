f1a820983fbd68864b8abd2ca2ed85b0d9ca7479
RodogInfinite <wjroland@gmail.com>
2024-05-11 21:28:03 -0400

    remove old method

--- src/lib.rs
+++ src/lib.rs
@@ -95,47 +95,6 @@ pub fn derive_macro(input: TokenStream) -> TokenStream {
 
         // Implement the parse method for the enum decorated with #[derive(TypeCast)]
         impl #name  {
-            // The parse method takes a mutable reference to a byte slice and returns an instance of DataKind
-            // pub fn parse(self, input: &mut &[u8]) -> #data_kind_name {
-            //     // Match the current variant of the enum decorated with #[derive(TypeCast)] and convert the input bytes accordingly
-            //     match self {
-            //         #(
-            //             #data_type_names::#variants => #data_kind_names::#variants ({
-            //                 // `bytes` should be the size of the entire data read in; therefore, the `rest` from the `split_at` can be ignored
-            //                 let (bytes, _) = input.split_at(
-            //                     std::mem::size_of::<#cast_types>()
-            //                 );
-            //                 <#cast_types>::#conversion(bytes.try_into().unwrap())
-            //             }),
-            //         )*
-            //         // Handle complex types
-            //         #(
-            //             #complex_data_type_names::#complex_variants => #complex_data_kind_names::#complex_variants ({
-            //                 let mut tmp_vec = std::vec::Vec::new();
-            //                 // Convert the byte information into the defined rust type from the cast. Push them to the temporary vector in this loop. This allows for an output array to be created that has the size of the array with the expected types defined in the cast.
-            //                 for _ in 0..#number_of_array_elements {
-            //                     let (bytes, rest) = input.split_at(
-            //                         std::mem::size_of::<#complex_cast_types>()
-            //                     );
-            //                     let converted = <#complex_cast_types>::#conversion(bytes.try_into().unwrap());
-            //                     // This allows the input to become the input bytes for the next iteration
-            //                     *input = rest;
-            //                     tmp_vec.push(converted);
-            //                 }
-            //                 // Transform the vec into the output array
-            //                 let out: [#complex_cast_types;#number_of_array_elements]  = tmp_vec.into_iter().collect::<Vec<#complex_cast_types>>().try_into().unwrap();
-            //                 out
-            //             }),
-            //         )*
-            //         // Handle string types
-            //         #(
-            //             #string_data_kind_names::#string_variants => #data_kind_name::#string_variants(String::from_utf8(input.to_vec()).unwrap())
-            //         )*
-
-            //     }
-
-            // }
-
             pub fn parse(self, input: &[u8]) -> IResult<&[u8], #data_kind_name> {
                 // Match the current variant of the enum decorated with #[derive(TypeCast)] and convert the input bytes accordingly
                 match self {
